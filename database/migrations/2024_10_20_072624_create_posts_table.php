705abea1df60911bde2d5ca8bd3289ef32c620af
unknown <henisalaiana714@gmail.com>
2024-11-06 10:29:44 +0700

    tugas update

---
+++ database/migrations/2024_10_20_072624_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kegiatan');
+            $table->string('category');
+            $table->date('deadline');
+            $table->integer('priority');
+            $table->string('status')->default('pending');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
